902a98d4bd97e00fb4f19fd00bbf333e326f6cb6
Xavier Martin Demers <xavier.pmd@outlook.com>
2021-10-21 19:38:00 -0400

    Adding more seeds to the database.

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -1,16 +1,50 @@
 -- Nike Items
 
 INSERT INTO items (name, description, price, sold, seller_id, product_photo_url, collection_name)
+VALUES('Nike Air T-Shirt (Blue)', 'Sky blue nike t-shirt from the air jordan collection.'
+
+, 39.99, false, 1, 'https://i5.walmartimages.com/asr/30ee76b0-e4d9-40a9-b7c9-14d1ba45c497_1.bce28595271eebe548192c4b2b950d41.jpeg', 'nike');
+
+INSERT INTO items (name, description, price, sold, seller_id, product_photo_url, collection_name)
+VALUES('Nike Joggers (Black & Grey)', 'Mixed-colour nike sport joggers'
+
+, 50.00, false, 2, 'http://static1.squarespace.com/static/532313ece4b08487acaec7a2/58659f84d1758e4aacc02021/5f32cfd33518380170bd9050/1600659580220/image+-+2020-08-11T130233.132.png', 'nike');
+
+INSERT INTO items (name, description, price, sold, seller_id, product_photo_url, collection_name)
+VALUES('Kyrie 5 (Cookies & Cream)', 'Basektball shoes from Kyries NBA line.'
+
+, 120.00, false, 1, 'https://i.pinimg.com/originals/05/85/53/05855300b65dd9cfadd6b4fa41c24d17.png', 'nike');
+
+INSERT INTO items (name, description, price, sold, seller_id, product_photo_url, collection_name)
 VALUES('Nike Fear of God 1 (Black)', 'Exclusive nike sneakers available for a limited time.'
 
 , 1050.00, false, 2, 'https://images.stockx.com/images/Nike-Air-Fear-Of-God-1-Black-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1606323579', 'nike');
 
-INSERT INTO items (name, description, price, sold, seller_id, product_photo_url, collection_name)
-VALUES('Nike Crew Neck Hoodie (Grey)', 'Stylish oversized nike hoodie.'
-
-, 59.99, false, 1, 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/tueblqxufluaep8yfmt2/sportswear-essential-womens-fleece-crew-7BNPDt.png', 'nike');
-
 -- Supreme Items
+
+INSERT INTO items (name, description, price, sold, seller_id, product_photo_url, collection_name)
+VALUES('Supreme Logo Hoodie (White)', 'Luxury supreme hoodie with an embroidered logo on the front.'
+
+, 465.00, false, 1, 'https://images.stockx.com/images/Supreme-Box-Logo-Hooded-Sweatshirt-White.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1611195320', 'supreme');
+
+INSERT INTO items (name, description, price, sold, seller_id, product_photo_url, collection_name)
+VALUES('Supreme Waist Bag (Red)', 'Mid-sized supreme bag from the SS20 week one drop.'
+
+, 825.00, false, 2, 'https://ofour.com/wp-content/uploads/2020/02/07-1.jpg', 'supreme');
+
+-- Chanel Items
+
+INSERT INTO items (name, description, price, sold, seller_id, product_photo_url, collection_name)
+VALUES('Supreme Logo Hoodie (White)', 'Luxury supreme hoodie with an embroidered logo on the front.'
+
+, 465.00, false, 1, 'https://images.stockx.com/images/Supreme-Box-Logo-Hooded-Sweatshirt-White.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1611195320', 'supreme');
+
+INSERT INTO items (name, description, price, sold, seller_id, product_photo_url, collection_name)
+VALUES('Supreme Waist Bag (Red)', 'Mid-sized supreme bag from the SS20 week one drop.'
+
+, 825.00, false, 2, 'https://ofour.com/wp-content/uploads/2020/02/07-1.jpg', 'supreme');
+
+-- Gucci Items
 
 INSERT INTO items (name, description, price, sold, seller_id, product_photo_url, collection_name)
 VALUES('Supreme Logo Hoodie (White)', 'Luxury supreme hoodie with an embroidered logo on the front.'
